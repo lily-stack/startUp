f32c0e6e73f26672a857e193ff82deaf00f394e9
Lily Hill <lillianmaihill@gmail.com>
2024-03-02 23:42:46 -0700

    post book

---
+++ HTML/JS/book.js
@@ -0,0 +1,12 @@
+
+function postBook() {
+    var author = document.getElementById("author");
+    var title = document.getElementById("title");
+    var rating = document.getElementById("rating");
+    addReview(author,  title,  rating);
+}
+
+
+
+
+
