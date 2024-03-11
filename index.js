9abd47fb917e66acf590aa1a77a6b015363d67cb
Lily Hill <lillianmaihill@gmail.com>
2024-03-11 16:06:43 -0600

    port and json parsing

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express');
 const app = express();
+
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+// JSON body parsing using built-in middleware
+app.use(express.json());
+
