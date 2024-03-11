b5cc7466952589c65812c821953ddf97a4faa5dd
Lily Hill <lillianmaihill@gmail.com>
2024-03-11 16:08:00 -0600

    frontend static hosting and router for service endpoints

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
+
+// Router for service endpoints
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
