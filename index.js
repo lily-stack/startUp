5ca74ff9d60e05643e5c0051fd26cf73e3e515ba
Lily Hill <lillianmaihill@gmail.com>
2024-03-20 14:52:15 -0600

    return if path unkown

--- index.js
+++ index.js
@@ -119,3 +119,8 @@ app.use(function (err, req, res, next) {
     res.status(500).send({ type: err.name, message: err.message });
 });
 
+// Return the application's default page if the path is unknown
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
+});
+
