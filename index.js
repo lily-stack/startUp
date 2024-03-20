ac07d7b8caf8ee4b299bfb1772ccbe79cf0f5967
Lily Hill <lillianmaihill@gmail.com>
2024-03-20 14:49:53 -0600

    change review backend

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
+const DB = require('./database.js');
+
+const authCookieName = 'token';
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -7,45 +12,91 @@ const port = process.argv.length > 2 ? process.argv[2] : 3000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
 // Serve up the frontend static content hosting
 app.use(express.static('public'));
 
-let reviews = [];
-let comments = [];
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
+
 // Router for service endpoints
 const apiRouter = express.Router();
 app.use(`/api`, apiRouter);
 
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await DB.createUser(req.body.email, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
+    }
+});
+
+// GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+});
+
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+});
+
+// GetUser returns information about a user
+apiRouter.get('/user/:email', async (req, res) => {
+    const user = await DB.getUser(req.params.email);
+    if (user) {
+      const token = req?.cookies.token;
+      res.send({ email: user.email, authenticated: token === user.token });
+      return;
+    }
+    res.status(404).send({ msg: 'Unknown' });
+});
+
+// secureApiRouter verifies credentials for endpoints
+var secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
+
 // GetReviews
-apiRouter.get('/review', (_req, res) => {
+apiRouter.get('/review', async (_req, res) => {
+    const reviews = await DB.getReviews();
     res.send(reviews);
 });
 
 // SubmitReview
-apiRouter.post('/review', (req, res) => {
+apiRouter.post('/review', async (req, res) => {
     //reviews = updateReviews(req.body, reviews);
-    const newReview = req.body;
-    reviews.push(newReview);
+    const review = { ...req.body, ip: req.ip };
+    await DB.addReview(review);
+    const reviews = await DB.getReviews();
     res.send(reviews);
 });
 
-// GetComments
-apiRouter.get('/comment', (_req, res) => {
-    res.send(comments);
-});
-
-// SubmitComment
-apiRouter.post('/comment', (req, res) => {
-    const newComment = req.body;
-    comments.push(newComment);
-    res.send(comments);
-});
-
-// Return the application's default page if the path is unknown
-app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-});
-  
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
--- notes.md
+++ notes.md
@@ -18,6 +18,8 @@ Deployment: use git bash not powershell
 
  ./deployFiles.sh -k ~/CS260/\$Reviewer260\$.pem -h shareyourbookshareyourthoughts.click -s startup
 
+ ./deployService.sh -k ~/CS260/\$Reviewer260\$.pem -h shareyourbookshareyourthoughts.click -s startup
+
  chmod 600 $Reviewer260$.pem     ------- change access for public 
  
 
