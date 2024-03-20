68b6f649348fa9fe04166c1524493f3b04cf2863
Lily Hill <lillianmaihill@gmail.com>
2024-03-20 14:58:55 -0600

    db variables

---
+++ database.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const config = require('./dbConfig.json');
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+
+const client = new MongoClient(url);
+const db = client.db('books');
+const userCollection = db.collection('user');
+const ratingCollection = db.collection('rating');
+const bookCollection = db.collection('book');
+const authorCollection = db.collection('author');
+const commentCollection = db.collection('comment');
+const commentBookCollection = db.collection('commentBook');
+
+(async function testConnection() {
+  await client.connect();
+  await db.command({ ping: 1 });
+})().catch((ex) => {
+  console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+  process.exit(1);
+});
