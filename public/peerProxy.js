f47c1b0e55dcd978b66854d5b867aba7a67cfe74
Lily Hill <lillianmaihill@gmail.com>
2024-03-25 10:21:45 -0600

    forward message

---
+++ public/peerProxy.js
@@ -0,0 +1,26 @@
+const { WebSocketServer } = require('ws');
+const uuid = require('uuid');
+
+function peerProxy(httpServer) {
+  // Create a websocket object
+  const wss = new WebSocketServer({ noServer: true });
+
+  // Handle the protocol upgrade from HTTP to WebSocket
+  httpServer.on('upgrade', (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, function done(ws) {
+      wss.emit('connection', ws, request);
+    });
+  });
+
+  // Keep track of all the connections so we can forward messages
+  let connections = [];
+
+  wss.on('connection', (ws) => {
+    const connection = { id: uuid.v4(), alive: true, ws: ws };
+    connections.push(connection);
+
+
+    });
+
+  };
+
