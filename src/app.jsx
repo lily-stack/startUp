9d1b2f44cc116ad03068942cf1635355d2badc32
Lily Hill <lillianmaihill@gmail.com>
2024-04-13 13:56:03 -0600

    view companents created and work

--- src/app.jsx
+++ src/app.jsx
@@ -9,48 +9,48 @@ import './app.css';
 export default function App() {
   return (
     <BrowserRouter>
-    <div className="bg-dark text-light">
+      <div className="body bg-dark text-light">
         <header className="container-fluid">
-            <nav className="navbar fixed-top navbar-dark">
-                <div className="navbar-brand" href="#">
-                    Book Share and Review
-                </div>
-                <menu className="navbar-nav">
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to=''>
-                            Login
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" to='book'>
-                            BookFeed
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link active" to='about'>
-                            About
-                        </NavLink>
-                    </li>
-                </menu>
-            </nav>
+          <nav className="navbar fixed-top navbar-dark">
+            <div className="navbar-brand">Book Share and Review</div>
+            <menu className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to='/' activeClassName="active">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to='/book' activeClassName="active">
+                  BookFeed
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to='/about' activeClassName="active">
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
         </header>
-        <Routes>
+
+        <div className="content">
+          <Routes>
             <Route path='/' element={<Login />} exact />
             <Route path='/book' element={<Book />} />
             <Route path='/about' element={<About />} />
             <Route path='*' element={<NotFound />} />
-        </Routes>
-        <footer className="bg-dark text-white-50">
-            <div className="container-fluid">
-                <span className="text-reset">Lillian Hill</span>
-                <a className="text-reset" href="https://github.com/lily-stack/startUp">Source</a>
-            </div>
+          </Routes>
+        </div>
+
+        <footer className="container-fluid bg-dark text-white-50 footer">
+            <span>Lillian Hill</span>
+            <a href="https://github.com/lily-stack/startUp">Source</a>
         </footer>
-    </div>
+      </div>
     </BrowserRouter>
-  )
+  );
 }
 
 function NotFound() {
-    return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
 }
