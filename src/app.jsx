afc8d2d0eb712a7fa1602be6acb740034c04b0b8
Lily Hill <lillianmaihill@gmail.com>
2024-04-13 11:45:01 -0600

    change a to navLink

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Book } from './book/book';
+import { About } from './about/about';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
@@ -9,9 +13,9 @@ export default function App() {
             <nav className="navbar fixed-top navbar-dark">
                 <a className="navbar-brand" href="#">Book Share and Review</a>
                 <menu className="navbar-nav">
-                <li className="nav-item"><a className="nav-link" href="index.html">Home</a></li>
-                <li className="nav-item"><a className="nav-link" href="book.html">BookFeed</a></li>
-                <li className="nav-item"><a className="nav-link active" href="about.html">About</a></li>
+                <li className="nav-item"><NavLink className="nav-link" href="index.html">Home</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link" href="book.html">BookFeed</NavLink></li>
+                <li className="nav-item"><NavLink className="nav-link active" href="about.html">About</NavLink></li>
                 </menu>
             </nav>
         </header>
@@ -19,7 +23,7 @@ export default function App() {
         <footer className="bg-dark text-white-50">
             <div className="container-fluid">
                 <span className="text-reset">Lillian Hill</span>
-                <a className="text-reset" href="https://github.com/lily-stack/startUp">Source</a>
+                <NavLink className="text-reset" href="https://github.com/lily-stack/startUp">Source</NavLink>
             </div>
         </footer>
     </body>
